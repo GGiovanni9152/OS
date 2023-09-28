cd131e40dffd9b6f96ef45ffc838f9ef4a4fd1ae
GGiovanni9152 <128748316+GGiovanni9152@users.noreply.github.com>
2023-09-28 23:48:51 +0300

    Add files via upload

---
+++ Laba1/laba.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+void error_checking(int result, char* error)
+{
+    if (result == -1)
+    {
+        printf("%s\n", error);
+        exit(-1);
+    }
+}
+
+char* get_string()
+{
+    int string_length = 0;
+    int capacity = 1;
+    char* string = (char*) malloc(sizeof(char));
+    char symbol = getchar();
+
+    if (symbol == EOF)
+    {
+        return NULL;
+    }
+
+    if (symbol == '\n')
+    {
+        symbol = getchar();
+    }
+
+    while (symbol != '\n')
+    {
+        string[string_length] = symbol;
+        ++string_length;
+        
+        if (string_length >= capacity)
+        {
+            capacity *= 2;
+            string = (char*) realloc(string, capacity * sizeof(char));
+        }
+
+        symbol = getchar();
+    }
+
+    string[string_length] = '\0';
+    return string;
+}
+
+
+int main()
+{
+    int first_pipe_fd[2];
+    int second_pipe_fd[2];
+    
+    error_checking(pipe(first_pipe_fd), "Pipe creating error");
+    error_checking(pipe(second_pipe_fd), "Pipe creating error");
+    
+    char* first_filename;
+    first_filename = get_string();
+    
+    char* second_filename;
+    second_filename = get_string();
+
+    int first_file, second_file;
+    
+    error_checking(first_file = open(first_filename, O_WRONLY | O_CREAT, 0777), "File openning error");
+    error_checking(second_file = open(second_filename, O_WRONLY | O_CREAT, 0777), "File openning error");
+
+    pid_t first_process_id = fork();
+    error_checking(first_process_id, "Fork error");
+    
+    if (first_process_id == 0)
+    {
+        pid_t second_process_id = fork();
+        error_checking(second_process_id, "Fork error");
+        
+        if (second_process_id == 0)
+        {
+            printf("Second child process\n");
+            close(second_pipe_fd[1]);
+            close(first_pipe_fd[0]);
+            close(first_pipe_fd[1]);
+            error_checking(dup2(second_pipe_fd[0], fileno(stdin)), "dup2 error");
+            close(second_pipe_fd[0]);
+            error_checking(dup2(second_file, fileno(stdout)), "dup2 error");
+            close(second_file);
+            char* const* argv = NULL;
+            error_checking(execv("child.out", argv), "Execv error");   
+		}
+		
+		else
+		{
+		    printf("First child process\n");
+		    close(first_pipe_fd[1]);
+            close(second_pipe_fd[0]);
+            close(second_pipe_fd[1]);
+            error_checking(dup2(first_pipe_fd[0], fileno(stdin)), "dup2 error");
+            close(first_pipe_fd[0]);
+            error_checking(dup2(first_file, fileno(stdout)), "dup2 error");
+            close(first_file);
+            char* const* argv = NULL;
+            error_checking(execv("child.out", argv), "Execv error");  
+            
+		}
+    }
+    
+    else
+    {
+        printf("Parent process\n");
+        close(first_pipe_fd[0]);
+        close(second_pipe_fd[0]);
+        char* input_string;
+        while ((input_string = get_string()) != NULL)
+        {
+            int string_length = strlen(input_string); 
+            int random_number = (rand() % 10) + 1;
+            if (random_number <= 8)
+            {
+                write(first_pipe_fd[1], &string_length, sizeof(int));
+                write(first_pipe_fd[1], input_string, sizeof(char) * string_length);
+            }
+            
+            else
+            {
+                write(second_pipe_fd[1], &string_length, sizeof(int));
+                write(second_pipe_fd[1], input_string, sizeof(char) * string_length);
+            }
+        }
+        close(first_pipe_fd[1]);
+        close(second_pipe_fd[1]);
+        close(first_file);
+        close(second_file);
+    }
+    
+    return 0;
+}
---
+++ Laba1/laba_child.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+void error_checking(int result, char* error)
+{
+    if (result == -1)
+    {
+        printf("%s\n", error);
+        exit(-1);
+    }
+}
+
+
+int is_vowel(char symbol)
+{
+    symbol = tolower(symbol);
+    
+    if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u' || symbol == 'y')
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+
+char* delete_vowels(char input_string[], int input_string_size)
+{
+    int output_string_size = 0;
+    int capacity = 1;
+    char* output_string = (char*) malloc(sizeof(char));
+    
+    for (int index = 0; index < input_string_size; ++index)
+    {
+        if (!is_vowel(input_string[index]))
+        {
+            output_string[output_string_size] = input_string[index];
+            ++output_string_size;
+            
+            if (output_string_size >= capacity)
+            {
+                capacity *= 2;
+                output_string = (char*) realloc(output_string, capacity * sizeof(char));
+            }
+        }
+    }
+    output_string[output_string_size] = '\0';
+    return output_string;
+}
+
+
+int main()
+{
+
+    int input_size;
+    while (read(fileno(stdin), &input_size, sizeof(int)) != 0)
+    {
+        char input_string[input_size];
+        read(fileno(stdin), &input_string, sizeof(char) * input_size);
+        char* new_string;
+        new_string = delete_vowels(input_string, input_size);
+        write(fileno(stdout), new_string, sizeof(char) * strlen(new_string));
+        write(fileno(stdout), "\n", sizeof(char));
+    }
+    
+    
+    return 0;
+}
